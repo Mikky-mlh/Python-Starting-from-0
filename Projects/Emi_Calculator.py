078b6ce41c9bfbdad235b6f15fdb59a812acfe74
Mikky-mlh <yuvrajsarathe07@gmail.com>
2025-10-23 17:31:44 +0530

    Emi_Calculator

--- Projects/Emi_Calculator.py
+++ Projects/Emi_Calculator.py
@@ -1,15 +1,34 @@
+""" 
+This is an Emi_Calculator.
+This code calculates the EMI a student will need to pay after taking an education loan. 
+This takes care of all the variables like principle amount, repayment and moratorium period, interest rate etc.
+"""
+
 p = int(input("Principal: "))
 r = float(input("Rate: "))
 m = int(input("Moratorium period (in months): "))
 t = int(input("Time exclusive of moratorium: "))
 family_income = float(input("Family income: "))
-
-S = p*r*m/1200 + p
-C = p*(1 + r/1200)**(t)
-c_emi = p * (r/1200) * ((1 + r/1200)**t) / (((1 + r/1200)**t) - 1)
-X = (C+S)/(m+t)
+r_m = r/1200
+S = p*r_m*m
+S_amt = S + p
 
 if family_income <= 450000:
-    print("EMI: ", c_emi)
+    P_eff = p
 else:
-    print("EMI: ", X)
+    P_eff = S_amt
+
+if t <= 0:
+    print("Error: Time period must be greater than 0")
+    exit()
+
+if r_m == 0:
+    EMI = P_eff / t
+else:
+    denominator = ((1 + r_m)**t) - 1
+    if denominator == 0:
+        print("Error: Invalid calculation parameters")
+        exit()
+    EMI = P_eff * r_m * ((1 + r_m)**t) / denominator
+
+print("EMI: ", EMI)
